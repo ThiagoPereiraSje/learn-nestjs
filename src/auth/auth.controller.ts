abadae1c88c614910ad0935f2bad4e9e8a543adc
Thiago Pereira <thiagopereiramail@gmail.com>
2022-04-04 18:21:36 +0000

    Create the first data transfer object. AuthDTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,25 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDTO } from './dto';
 
 // Controllers who handle the requests
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  // Post request to /auth/signup
+  @Post('signup')
+  signup(@Body() dto: AuthDTO) {
+    console.log({
+      dto,
+    });
+
+    return this.authService.signup();
+  }
+
   // Post request to /auth/signin
   @Post('signin')
   signin() {
     return this.authService.signin();
-  }
-
-  // Post request to /auth/signup
-  @Post('signup')
-  signup() {
-    return this.authService.signup();
   }
 }
