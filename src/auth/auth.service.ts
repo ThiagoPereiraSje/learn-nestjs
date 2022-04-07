d9e44bd07781a3d27f9ffbfbb54e3e5ad5955c43
Thiago Pereira <thiagopereiramail@gmail.com>
2022-04-07 13:29:38 +0000

    Handle duplicate credentials error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDTO } from './dto';
 import { hash } from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 // Services who handle the business logic
 @Injectable({})
@@ -12,18 +13,28 @@ export class AuthService {
     // generate the password hash
     const password_hash = await hash(dto.password);
 
-    // save the new user in the db
-    const user = await this.prisma.user.create({
-      data: { email: dto.email, hash: password_hash },
-      select: {
-        id: true,
-        email: true,
-        createdAt: true,
-      },
-    });
+    try {
+      // save the new user in the db
+      const user = await this.prisma.user.create({
+        data: { email: dto.email, hash: password_hash },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
 
-    // return the saved user
-    return user;
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+
+      throw error;
+    }
   }
 
   signin() {
